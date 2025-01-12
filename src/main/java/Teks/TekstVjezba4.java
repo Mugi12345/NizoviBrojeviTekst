ffbb020046b220a486da23523f97c0d5204091a2
Filip Mugosa <filipmugosa7@gmail.com>
2025-01-12 19:33:39 +0100

    Tekst

---
+++ src/main/java/Teks/TekstVjezba4.java
@@ -0,0 +1,14 @@
+package Teks;
+
+public class TekstVjezba4 {
+    public static void main(String[] args) {
+        String[] forbidenChars = { "<", ">", "'" };
+        String input = "<div>my 'div' tag</div>";
+
+        String output = input;
+        for (int i = 0; i < forbidenChars.length; i++) {
+           output = output.replace(forbidenChars[i], "");
+        }
+        System.out.println(output);
+    }
+}
